a4a6f3a78cd7bf6b7bdc27c2ca27db8c2da4e665
ObscuritySRL <stev.p@outlook.com>
2021-11-17 16:11:23 -0500

    Added ContextMenu and ContextMenuTargetResolver

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,8 @@ export { default as FastifyManager } from './managers/FastifyManager';
 export { default as Attachment } from './structures/Attachment';
 export { default as Command } from './structures/Command';
 export { default as CommandOptionResolver } from './structures/CommandOptionResolver';
+export { default as ContextMenu } from './structures/ContextMenu';
+export { default as ContextMenuTargetResolver } from './structures/ContextMenuTargetResolver';
 export { default as Embed } from './structures/Embed';
 export { default as Member } from './structures/GuildMember';
 export { default as Message } from './structures/Message';
